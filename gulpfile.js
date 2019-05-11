9b5daf36ee0b27688c711d150b02f9ec36a16782
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2019-05-11 02:43:26 -0500

    Create _gulpfile.js_ with gulp tasks for compile core and browser-sync, add *gulp*, *google-closure-compiler* and *browser-sync* devDependencies and create _.gitignore_

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp        = require('gulp');
+const cp          = require('child_process');
+const browserSync = require('browser-sync').create();
+
+gulp.task('compile-core', (done) => {
+    const outputFilePath = 'res/js/Draw2PNG/dist/Draw2PNG.min.js'; 
+    const folders        = [
+        // Order matters:
+        'res/js/Draw2PNG/src/functions/*.js', 
+        'res/js/Draw2PNG/src/classes/*.js', 
+        'res/js/Draw2PNG/src/Color.js', 
+        'res/js/Draw2PNG/src/Pixmap.js', 
+        'res/js/Draw2PNG/src/Filter.js', 
+        'res/js/Draw2PNG/src/ColorToleranceFilter.js'
+    ].join(' ');
+
+    cp.execSync(
+        './node_modules/.bin/google-closure-compiler ' + 
+        '--js ' + folders + 
+        ' --js_output_file ' + outputFilePath
+    );
+    done();
+});
+
+gulp.task('browser-sync', (done) => {
+    const port = 9999;
+    
+    browserSync.init({
+        port   : port, 
+        server : {
+            baseDir : './'
+        }
+    });
+});
