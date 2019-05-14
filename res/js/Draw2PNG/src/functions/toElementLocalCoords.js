ea5d3c67d8aaab22b24d78b483bfe526542d9a0f
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2019-05-14 13:33:38 -0500

    Fix newline trim in 'res/js/Draw2PNG/src/functions/toElementLocalCoords.js'

--- res/js/Draw2PNG/src/functions/toElementLocalCoords.js
+++ res/js/Draw2PNG/src/functions/toElementLocalCoords.js
@@ -1,1 +1,17 @@
-/** * Returns the local coordinates within a HTML element without offset. * * Idea taken from jerryj at http://www.html5canvastutorials.com/advanced/html5-canvas-mouse-coordinates/ */function toElementLocalCoords (element, clientX, clientY) {  if (!(element instanceof HTMLElement) || isNaN(clientX) || isNaN(clientY)) {    throw 'InvalidTypeError';  }    var rect = element.getBoundingClientRect();    return {    x : (clientX - rect.left) / (rect.right  - rect.left) * element.width,     y : (clientY - rect.top)  / (rect.bottom - rect.top)  * element.height  };}
+/**
+ * Returns the local coordinates within a HTML element without offset.
+ * 
+ * Idea taken from jerryj at http://www.html5canvastutorials.com/advanced/html5-canvas-mouse-coordinates/
+ */
+function toElementLocalCoords (element, clientX, clientY) {
+  if (!(element instanceof HTMLElement) || isNaN(clientX) || isNaN(clientY)) {
+    throw 'InvalidTypeError';
+  }
+  
+  var rect = element.getBoundingClientRect();
+  
+  return {
+    x : (clientX - rect.left) / (rect.right  - rect.left) * element.width, 
+    y : (clientY - rect.top)  / (rect.bottom - rect.top)  * element.height
+  };
+}
